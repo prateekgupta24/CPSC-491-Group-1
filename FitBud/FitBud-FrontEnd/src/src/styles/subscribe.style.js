866dbe994e5452a7fd037663b99a298c8ff6ab4e
Austin Sohn <56807941+austin-sohn@users.noreply.github.com>
2022-10-04 22:21:28 -0700

    Delete FitBud/FitBud-FrontEnd/src/src directory
    added to wrong place

--- FitBud/FitBud-FrontEnd/src/src/styles/subscribe.style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Sub = styled.div`
-  background-color: #282c34;
-  min-height: 100vh;
-  color: white;
-`;
-
-export default Sub;
--- FitBud/FitBud-FrontEnd/src/src/styles/userprofile.style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Profile = styled.div`
-  background-color: #282c34;
-  min-height: 100vh;
-  color: white;
-`;
-
-export default Profile;
