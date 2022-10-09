506e5b853b3f24d4a58ac237a7205cef655e9562
josearceiv <82137095+josearceiv@users.noreply.github.com>
2022-10-08 23:44:57 -0700

    Update subscribe page

--- FitBud/FitBud-FrontEnd/src/pages/subscribe.js
+++ FitBud/FitBud-FrontEnd/src/pages/subscribe.js
@@ -4,9 +4,15 @@ import Sub from '../styles/subscribe.style';
 const subscribe = () => {
   return(
     <Sub>
-      <div>
-        pls gib moni
-      </div>
+      <div className="subscribe">
+        <header className="subscribe-header">
+        <h1>Subscribe:</h1>
+        <p>1 month: $9.99</p>
+        <p>3 months: $24.99 ($4.99 off)</p>
+        <p>6 months: $49.99 ($9.99 off)</p>
+        <p>12 months: $89.99 ($29.99 off)</p>
+          </header>
+          </div>
     </Sub>
   );
 }
