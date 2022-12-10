25fe7a51d51433d8ce33fb608b290eeb61f86b1b
josearceiv <82137095+josearceiv@users.noreply.github.com>
2022-12-09 19:27:22 -0800

    updated userpreferences

--- src/pages/userpreferences.js
+++ src/pages/userpreferences.js
@@ -1,149 +1,200 @@
-import React, { useState, useEffect } from "react";
-import {
-  Preferences,
-  PreferencesTitle,
-  PreferencesForm,
-  PreferencesSection,
-  PreferencesInput,
-  PreferencesSelect,
-} from "../styles/userpreferences.style";
+import React from "react";
+import { Preferences, PreferencesForm, PreferencesTitle } from "../styles/userpreferences.style";
 import { useNavigate } from "react-router-dom";
 import { IconButton } from "@mui/material";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import Button from "@mui/material/Button/";
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import FormHelperText from "@mui/material/FormHelperText";
+import CssBaseline from "@mui/material/CssBaseline";
 import axios from "axios";
-
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
+ 
 const UserPreferences = () => {
-  const navigate = useNavigate();
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const data = {
-      city: event.target.city.value,
-      workoutstyle: event.target.lname.value,
-      age: event.target.age.value,
-      gender: event.target.gender.value,
-      height: event.target.heightft.value + "'" + event.target.heightin.value + '"',
-      weight: event.target.weight.value,
-      distance: event.target.distance.value,
-      gym: event.target.gym.value,
-    };
-    axios
-      .put("http://localhost:8080/preferences", data)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-        return;
-      });
-    navigate(-1);
-  };
-  
-
-  return (
-    <Preferences>
-      <IconButton
-        onClick={() => {
-          navigate(-1);
-        }}
-        color="primary"
-        aria-label="back"
-        component="label"
-        style={{ justifyContent: "flex-start" }}
-      >
-        <ArrowBackIosIcon />
-      </IconButton>
-
-      <PreferencesForm id="Form" onSubmit={handleSubmit}>
-        <PreferencesTitle id="Title">User Preferences</PreferencesTitle>
-        <PreferencesSection id="My-City">
-          <label>My City</label>
-          <PreferencesInput />
-        </PreferencesSection>
-
-        <PreferencesSection id="Workout-Style">
-          <label>Workout Style</label> <PreferencesInput />
-        </PreferencesSection>
-
-        <PreferencesSection id="Age">
-          <label>Age Range</label>
-          <PreferencesInput />
-        </PreferencesSection>
-
-        <PreferencesSection id="Gender">
-          <label>Interested in</label>
-          <PreferencesSelect>
-            <option value="" />
-            <option value="1">Male</option>
-            <option value="2">Female</option>
-            <option value="3">Other</option>
-          </PreferencesSelect>
-        </PreferencesSection>
-
-        <PreferencesSection id="Height">
-          <div className="Height-Foot">
-            <label>Height</label>
-            <PreferencesSelect>
-              <option value="" />
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-            </PreferencesSelect>
-            <label>ft</label>
-          </div>
-          <div className="Height-Inches">
-            <PreferencesSelect>
-              <option value="" />
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-              <option value="11">11</option>
-            </PreferencesSelect>
-            <label>in</label>
-          </div>
-        </PreferencesSection>
-
-        <PreferencesSection id="Weight">
-          <label>Weight</label>
-          <PreferencesInput />
-          <PreferencesSelect>
-            <option value="" />
-            <option value="1">lbs</option>
-            <option value="2">kgs</option>
-          </PreferencesSelect>
-        </PreferencesSection>
-
-        <PreferencesSection id="Distance">
-          <label>Distance</label>
-          <PreferencesInput />
-          <PreferencesSelect>
-            <option value="" />
-            <option value="1">miles</option>
-            <option value="2">kilometers</option>
-          </PreferencesSelect>
-        </PreferencesSection>
-
-        <PreferencesSection id="Gym">
-          <label>Gym</label> <PreferencesInput />
-        </PreferencesSection>
-        <Button variant="contained">Save</Button>
-      </PreferencesForm>
-    </Preferences>
-  );
+ const navigate = useNavigate();
+ 
+ //const [post, setPost] = useState(null);
+ const darkTheme = createTheme({
+   palette: {
+     mode: "dark",
+   },
+ });
+ 
+ const handleSubmit = (event) => {
+   event.preventDefault();
+   // somehow get email
+   const data = {
+     myCity: event.target.myCity.value,
+     age: event.target.age.value,
+     gender: event.target.gender.value,
+     height:
+       event.target.heightft.value + "'" + event.target.heightin.value + '"',
+     weight: event.target.weight.value,
+     distance: event.target.distance.value,
+     workout: event.target.city.value,
+     gym: event.target.gym.value,
+   };
+   axios
+     .put("http://localhost:8080/userpreferences", data)
+     .then((response) => {
+       console.log(response.data);
+     })
+     .catch((error) => {
+       console.log(error);
+       return;
+     });
+   navigate(-1);
+ };
+ 
+ return (
+   <Preferences>
+     <ThemeProvider theme={darkTheme}>
+       <CssBaseline />
+       <IconButton
+         onClick={() => {
+           navigate(-1);
+         }}
+         color="primary"
+         aria-label="back"
+         component="label"
+         style={{ justifyContent: "flex-start" }}
+       >
+         <ArrowBackIosIcon />
+       </IconButton>
+       <PreferencesForm onSubmit={handleSubmit}>
+       <PreferencesTitle id="Title">User Preferences</PreferencesTitle>
+         <Box
+           component="PreferencesForm"
+           sx={{
+             "& .MuiTextField-root": { m: 1, width: "25ch" },
+           }}
+           noValidate
+           autoComplete="off"
+         >
+           <div id="my-city">
+             <TextField
+               id="outlined-myCity"
+               type="text"
+               label="My City"
+               name="myCity"
+             />
+           </div>
+           <div id="age">
+             <TextField
+               style={{ width: 75 }}
+               id="outlined-age"
+               label="Age Range"
+               type="number"
+               InputLabelProps={{
+                 shrink: true,
+               }}
+               variant="standard"
+               name="age"
+             />
+           </div>
+           <div id="gender">
+             <FormControl sx={{ m: 1, width: "17ch" }}>
+               <InputLabel id="gender-select-label">Interested In</InputLabel>
+               <Select
+                 labelId="gender-select-label"
+                 id="gender-select"
+                 label="Gender"
+                 name="gender"
+               >
+                 <MenuItem value={"male"}>Male</MenuItem>
+                 <MenuItem value={"female"}>Female</MenuItem>
+                 <MenuItem value={"other"}>Other</MenuItem>
+               </Select>
+             </FormControl>
+           </div>
+           <div id="height">
+             <FormHelperText sx={{ m: 1 }} id="outlined-height-helper-text">
+               Height
+             </FormHelperText>
+             <FormControl sx={{ m: 1, width: "10ch" }}>
+               <OutlinedInput
+                 id="outlined-adornment-feet"
+                 endAdornment={
+                   <InputAdornment position="end">Ft</InputAdornment>
+                 }
+                 aria-describedby="outlined-feet-helper-text"
+                 inputProps={{
+                   "aria-label": "height-feet",
+                 }}
+                 name="heightft"
+               />
+             </FormControl>
+             <FormControl sx={{ m: 1, width: "10ch" }}>
+               <OutlinedInput
+                 id="outlined-adornment-feet"
+                 endAdornment={
+                   <InputAdornment position="end">In</InputAdornment>
+                 }
+                 aria-describedby="outlined-inch-helper-text"
+                 inputProps={{
+                   "aria-label": "height-inch",
+                 }}
+                 name="heightin"
+               />
+             </FormControl>
+           </div>
+ 
+           <FormControl sx={{ m: 1, width: "10ch" }}>
+             <div id="weight">
+               <FormHelperText id="outlined-weight-helper-text">
+                 Weight
+               </FormHelperText>
+               <OutlinedInput
+                 id="outlined-adornment-weight"
+                 endAdornment={
+                   <InputAdornment position="end">lbs</InputAdornment>
+                 }
+                 aria-describedby="outlined-weight-helper-text"
+                 inputProps={{
+                   "aria-label": "weight",
+                 }}
+                 name="weight"
+               />
+             </div>
+           </FormControl>
+           <div id="distance">
+             <TextField
+               style={{ width: 75 }}
+               id="outlined-distance"
+               label="Distance"
+               type="number"
+               InputLabelProps={{
+                 shrink: true,
+               }}
+               variant="standard"
+               name="distance"
+             />
+           </div>
+           <div id="workout">
+             <TextField id="outlined-workout" label="Workout Style" name="workout" />
+           </div>
+           <div id="gym">
+             <TextField id="outlined-gym" type="text" label="Gym" name="gym" />
+           </div>
+         </Box>
+         <Button
+           type="submit"
+           variant="contained"
+           style={{ marginLeft: "8px" }}
+         >
+           Save
+         </Button>
+       </PreferencesForm>
+     </ThemeProvider>
+   </Preferences>
+ );
 };
-
+ 
 export default UserPreferences;
