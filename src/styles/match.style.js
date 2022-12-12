83472411128db5e8fa14ae2e519c856f4470cff8
austin-sohn <sohn.austin@gmail.com>
2022-12-11 22:57:53 -0800

    changed subscription to match

--- src/styles/match.style.js
+++ src/styles/match.style.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
-const SubscriptionStyle = styled.div`
+const MatchStyle = styled.div`
   background-color: #101425;
   min-height: 100vh;
   color: white;
   scroll-margin-top: 60.5px;
 `;
 
-export default SubscriptionStyle;
+export default MatchStyle;
